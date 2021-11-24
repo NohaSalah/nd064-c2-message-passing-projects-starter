0d7f0044c351c0456f9484f367b7e97ebcc9740a
nohasalah <nohasalah.iti@gmail.com>
2021-11-24 22:09:43 +0200

    working on proto folder

--- modules/PersonsAPI/app/__init__.py
+++ modules/PersonsAPI/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.exc import NoResultFound
+from werkzeug.exceptions import InternalServerError
 
 db = SQLAlchemy()
 
@@ -12,7 +14,7 @@ def create_app(env=None):
 
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
-    api = Api(app, title="UdaConnect API", version="0.1.0")
+    api = Api(app, title="UdaConnect API", version="0.1.1")
 
     CORS(app)  # Set CORS for development
 
@@ -22,5 +24,16 @@ def create_app(env=None):
     @app.route("/health")
     def health():
         return jsonify("healthy")
+
+
+    # error handling 
+    @app.errorhandler(NoResultFound)
+    def no_result_found(e):
+        return {'error': 'No Result Found To Return'}, 404
+
+    @app.errorhandler(InternalServerError)
+    def internal_server_error(e):
+        return {'erroor' : 'Internal Server Error','info' : e.original_exception }, 500
+                
 
     return app
