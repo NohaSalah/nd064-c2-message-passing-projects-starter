743ae21f8aa6b6b2cecfef98c4b27df6fa6d3160
nohasalah <nohasalah.iti@gmail.com>
2021-12-01 23:55:29 +0200

    create services

---
+++ modules/ConnectionData/app/udaconnect/services/locationServices.py
@@ -0,0 +1,38 @@
+import logging
+from datetime import datetime, timedelta
+from typing import Dict, List
+
+from app import db
+from app.udaconnect.models import Connection, Location, Person
+from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
+from geoalchemy2.functions import ST_AsText, ST_Point
+from sqlalchemy.sql import text
+
+
+
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("udaconnect-location-svc")
+              
+
+class LocationService:
+    @staticmethod
+    def retrieve(location_id) -> Location:
+        location, coord_text = (
+            db.session.query(Location, Location.coordinate.ST_AsText())
+            .filter(Location.id == location_id)
+            .one()
+        )
+
+        # Rely on database to return text form of point to reduce overhead of conversion in app code
+        location.wkt_shape = coord_text
+        return location
+
+    @staticmethod
+    def create(location: Dict):
+        validation_results: Dict = LocationSchema().validate(location)
+        if validation_results:
+            logger.warning(f"Unexpected data format in payload: {validation_results}")
+            raise Exception(f"Invalid payload: {validation_results}")
+        # from LocationProducer in services
+        LocationProducer.sendMsg(location)
