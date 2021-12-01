8cb0c1c538be1fa99d758ab6b140ebb733734fe4
NSTohamy <NohaSalah@users.noreply.github.com>
2021-12-01 21:02:52 +0200

    Update __init__.py
    Final file

--- modules/PersonsAPI/app/udaconnect/__init__.py
+++ modules/PersonsAPI/app/udaconnect/__init__.py
@@ -5,4 +5,4 @@ from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchem
 def register_routes(api, app, root="api"):
     from app.udaconnect.controllers import api as udaconnect_person_api
 
-    api.add_namespace(udaconnect_api, path=f"/{root}")
+    api.add_namespace(udaconnect_person_api, path=f"/{root}")
