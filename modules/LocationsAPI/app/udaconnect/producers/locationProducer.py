8ed9b620d956ce18b1ae521fe65f08781bf343ed
nohasalah <nohasalah.iti@gmail.com>
2021-12-01 20:18:24 +0200

    try to build it

---
+++ modules/LocationsAPI/app/udaconnect/producers/locationProducer.py
@@ -0,0 +1,31 @@
+import logging
+import json
+
+from kafka import 
+
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("udaconnect-location-svc")
+              
+
+class LocationService:
+    @staticmethod
+    def retrieve(location_id) -> Location:
+        location, coord_text = (
+            db.session.query(Location, Location.coordinate.ST_AsText())
+            .filter(Location.id == location_id)
+            .one()
+        )
+
+        # Rely on database to return text form of point to reduce overhead of conversion in app code
+        location.wkt_shape = coord_text
+        return location
+
+    @staticmethod
+    def create(location: Dict):
+        validation_results: Dict = LocationSchema().validate(location)
+        if validation_results:
+            logger.warning(f"Unexpected data format in payload: {validation_results}")
+            raise Exception(f"Invalid payload: {validation_results}")
+
+        LocationProducer.send_message(location)
