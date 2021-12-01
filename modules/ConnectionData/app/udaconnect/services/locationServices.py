531c943cd469c20e87fd11bb53c90bbb04b98b42
nohasalah <nohasalah.iti@gmail.com>
2021-12-02 00:15:44 +0200

    enhance

--- modules/ConnectionData/app/udaconnect/services/locationServices.py
+++ modules/ConnectionData/app/udaconnect/services/locationServices.py
@@ -1,38 +1,19 @@
-import logging
-from datetime import datetime, timedelta
 from typing import Dict, List
+from builtins import staticmethod
+from functools import lru_cache
 
-from app import db
-from app.udaconnect.models import Connection, Location, Person
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
-from geoalchemy2.functions import ST_AsText, ST_Point
-from sqlalchemy.sql import text
+from app.udaconnect.models import Location
+from app.udaconnect.infra.database import DBSession
 
-
-
-
-logging.basicConfig(level=logging.WARNING)
-logger = logging.getLogger("udaconnect-location-svc")
-              
+session = DBSession()
 
 class LocationService:
+    @lru_cache(maxsize=10)
     @staticmethod
-    def retrieve(location_id) -> Location:
-        location, coord_text = (
-            db.session.query(Location, Location.coordinate.ST_AsText())
-            .filter(Location.id == location_id)
-            .one()
-        )
-
-        # Rely on database to return text form of point to reduce overhead of conversion in app code
-        location.wkt_shape = coord_text
-        return location
-
-    @staticmethod
-    def create(location: Dict):
-        validation_results: Dict = LocationSchema().validate(location)
-        if validation_results:
-            logger.warning(f"Unexpected data format in payload: {validation_results}")
-            raise Exception(f"Invalid payload: {validation_results}")
-        # from LocationProducer in services
-        LocationProducer.sendMsg(location)
+    def fetchLocations(person_id, start_date, end_date) -> List:
+        locations : List = (
+                        session.query(Location).filter(Location.person_id == person_id)
+                        .filter(Location.creation_time < end_date)
+                        .filter(Location.creation_time >= start_date).all()
+                        )
+        return locations
